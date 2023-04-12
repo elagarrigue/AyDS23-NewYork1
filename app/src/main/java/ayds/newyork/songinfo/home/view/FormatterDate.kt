ded006a450e031d7427e4171c59b2162a7352b84
rocio <rocioyatzky@gmail.com>
2023-04-12 17:14:35 -0300

    Arreglando fechas y formatos

--- app/src/main/java/ayds/newyork/songinfo/home/view/FormatterDate.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/FormatterDate.kt
@@ -1,15 +1,12 @@
 package ayds.newyork.songinfo.home.view
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import java.time.LocalDate
-import java.time.Year
-import java.time.YearMonth
-import java.util.Date.*
+import java.text.SimpleDateFormat
 
+
+@Suppress("DEPRECATION")
 class FormatterDate {
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     fun viewRelease (releaseDate: String, precision: String): String {
         return when (precision) {
             "day" -> dayString(releaseDate)
@@ -19,21 +16,42 @@ class FormatterDate {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun dayString (date: String) : String {
-        val day = LocalDate.parse(date)
-        return day.dayOfMonth.toString() + "/" + day.monthValue.toString() + "/" + day.year.toString()
-    }
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun monthString (date: String) : String {
-        val month = YearMonth.parse(date)
-        return month.month.toString() +", " + month.year.toString()
+        private fun dayString (date: String) : String {
+        // val day = LocalDate.parse(date)
+        println(date)
+        val fecha = SimpleDateFormat("yyyy-MM-dd")
+            val print = fecha.parse(date)
+        println(print.date.toString() + "/" + print.month.inc().toString() + "/" + print.year.plus(1900).toString())
+
+
+        return  print.date.toString() + "/" + print.month.inc().toString() + "/" + print.year.plus(1900).toString()
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+    private fun monthString (date: String) : String {
+        println(date)
+        var formatMes = SimpleDateFormat("MM")
+        val fecha = formatMes.parse(date)
+        formatMes = SimpleDateFormat("MMMM")
+        println(buildString {
+        append("fecha: ")
+        append(fecha)
+        })
+        val month = formatMes.format(fecha)
+        val formatAnio = SimpleDateFormat("yyyy")
+        val fecha2 = formatAnio.parse(date)
+        println(buildString {
+            append("fecha2: ")
+            append(fecha2)
+        })
+        val year = formatAnio.format(fecha2)
+        println("$month, $year")
+
+        return "$month, $year"
+    }
+
     private fun yearString (date: String) : String {
-        val year = Year.parse(date)
-        return year.toString() + " ( " + isLeapYear(date) + " )"
+        //val year = Year.parse(date)
+        return "" //year.toString() + " ( " + isLeapYear(date) + " )"
     }
     private fun isLeapYear (year: String) : String {
         val yearInt = year.toInt()
