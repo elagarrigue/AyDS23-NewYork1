141d5828851b57e63c158e4649b8b20494b48cbc
rocio <rocioyatzky@gmail.com>
2023-05-26 18:32:57 -0300

    update

--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/ArtistInfoRepositoryImpl.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/ArtistInfoRepositoryImpl.kt
@@ -3,34 +3,54 @@ package ayds.newyork.songinfo.moredetails.data
 import ayds.newyork.songinfo.moredetails.domain.ArtistInfoRepository
 import ayds.newyork.songinfo.moredetails.domain.ArtistInformation
 import ayds.newyork.songinfo.moredetails.data.local.sqldb.ArtistInfoLocalStorageImpl
-
+import com.example.nyt1_module.DependenciesInjector
+import com.example.nyt1_module.NYTArtistInfoService
+import com.example.nyt1_module.entity.ArtistInformationExternal
 
 class ArtistInfoRepositoryImpl(
     private val artistInfoLocalStorage: ArtistInfoLocalStorageImpl,
-    private val artistInfoExternalStorage: NYTimes,
+    private var artistInfoExternalStorage: NYTArtistInfoService,
+    private var artistInfo: ArtistInformation?,
 ) : ArtistInfoRepository {
 
     override fun getArtistInfoByTerm(name: String): ArtistInformation? {
-        var artistInfo = artistInfoLocalStorage.getArtistInfo(name)
+        var artistInfoLocal = artistInfoLocalStorage.getArtistInfo(name)
 
-        when {
-            artistInfo is ArtistInformation.ArtistInformationData -> markArtistInfoAsLocal(
-                artistInfo
-            )
+        when (artistInfoLocal) {
+            is ArtistInformation.ArtistInformationData -> {
+                markArtistInfoAsLocal(artistInfoLocal)
+                artistInfo = artistInfoLocal
+            }
             else -> {
                 try {
-                    artistInfo = artistInfoExternalStorage.getArtistInfo(name)
-                    artistInfoLocalStorage.saveArtistInfo(artistInfo as ArtistInformation.ArtistInformationData)
+                    artistInfoExternalStorage = DependenciesInjector.init()
+                    var artistInfoExternal = artistInfoExternalStorage.getArtistInfo(name)
+                    when (artistInfoExternal) {
+                        is ArtistInformationExternal.ArtistInformationDataExternal -> {
+                            val artistInfoData = ArtistInformation.ArtistInformationData(
+                                artistInfoExternal.artistName,
+                                artistInfoExternal.abstract,
+                                artistInfoExternal.url,
+                                artistInfoExternal.isLocallyStored
+                            )
+                            artistInfoLocalStorage.saveArtistInfo(artistInfoData)
+                            artistInfo = artistInfoData
+                        }
+                        else -> {
+                            artistInfo = ArtistInformation.ArtistInformationEmpty
+                        }
+                    }
                 } catch (e: Exception) {
                     artistInfo = ArtistInformation.ArtistInformationEmpty
                 }
             }
         }
-
         return artistInfo
     }
 
     private fun markArtistInfoAsLocal(artistInfo: ArtistInformation.ArtistInformationData) {
         artistInfo.isLocallyStored = true
     }
+
+
 }
