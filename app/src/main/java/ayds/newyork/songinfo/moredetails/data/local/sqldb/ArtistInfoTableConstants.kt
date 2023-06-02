75f59e91dfefd08e818ae9ce1eeecb01b4c82355
agusdeba <agusdebattista@gmail.com>
2023-06-02 11:35:17 -0300

    correcciones

--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/local/sqldb/ArtistInfoTableConstants.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/local/sqldb/ArtistInfoTableConstants.kt
@@ -1,7 +1,7 @@
 package ayds.newyork.songinfo.moredetails.data.local.sqldb
 
 
-const val TABLE_NAME = "cards"
+const val TABLE_NAME = "artists"
 const val COLUMN_ID = "id"
 const val COLUMN_ARTIST_NAME = "artist_name"
 const val COLUMN_DESCRIPTION = "description"
@@ -12,6 +12,7 @@ const val DATABASE_NAME = "dictionary.db"
 const val DATABASE_VERSION = 1
 const val CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " +
         "$TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        "$COLUMN_ARTIST_NAME TEXT NOT NULL, " +
         "$COLUMN_DESCRIPTION TEXT NOT NULL, $COLUMN_INFO_URL TEXT NOT NULL," +
         "$COLUMN_SOURCE INTEGER NOT NULL, " +
         "$COLUMN_SOURCE_URL TEXT)"
